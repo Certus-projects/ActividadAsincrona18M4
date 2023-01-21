1dd271928e59da5f190476f06c2f3bc10c983bfc
Jhon Bellido <jhonbellidovargas@gmail.com>
2023-01-21 11:35:29 -0500

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,24 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'actividad-9';
+  email: string | undefined;
+  password: string | undefined;
+  response: any;
+
+  constructor(private http: HttpClient) {}
+
+  submit() {
+    this.http
+      .post('https://reqres.in/api/registerUser', {
+        email: this.email,
+        password: this.password,
+      })
+      .subscribe((response) => (this.response = response));
+  }
 }
